f7494e803d5783e35ac5add023b5ff2a4a1bb6ec
Chris Athanas <realityexpander@gmail.com>
2022-08-07 04:43:05 -0500

    Fix naming

---
+++ app/src/main/java/com/realityexpander/bikingapp/di/ServiceModule.kt
@@ -0,0 +1,57 @@
+package com.realityexpander.bikingapp.di
+
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import androidx.core.app.NotificationCompat
+import com.realityexpander.bikingapp.R
+import com.realityexpander.bikingapp.other.Constants
+import com.realityexpander.bikingapp.ui.MainActivity
+import com.google.android.gms.location.FusedLocationProviderClient
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ServiceComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.android.scopes.ServiceScoped
+
+/**
+ * ServiceModule, provides dependencies for the TrackingService
+ */
+@Module
+@InstallIn(ServiceComponent::class)
+object ServiceModule {
+
+    @ServiceScoped
+    @Provides
+    fun providesFusedLocationProviderClient(
+        @ApplicationContext context: Context
+    ) = FusedLocationProviderClient(context)
+
+    @ServiceScoped
+    @Provides
+    fun provideBaseNotificationBuilder(
+        @ApplicationContext context: Context,
+        pendingIntent: PendingIntent
+    ) = NotificationCompat.Builder(context, Constants.NOTIFICATION_CHANNEL_ID)
+        .setAutoCancel(false)
+        .setOngoing(true)
+        .setSmallIcon(R.drawable.ic_directions_run_black_24dp)
+        .setContentTitle("Running App")
+        .setContentText("00:00:00")
+        .setContentIntent(pendingIntent)
+
+    @ServiceScoped
+    @Provides
+    fun provideActivityPendingIntent(
+        @ApplicationContext context: Context
+    ) =
+        PendingIntent.getActivity(
+            context,
+            0,
+            Intent(context, MainActivity::class.java).apply {
+                action = Constants.ACTION_SHOW_TRACKING_FRAGMENT
+            },
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+}
---
+++ app/src/main/java/com/realityexpander/bikingapp/ui/fragments/TrackingFragment.kt
@@ -0,0 +1,337 @@
+package com.realityexpander.bikingapp.ui.fragments
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import com.realityexpander.bikingapp.R
+import com.realityexpander.bikingapp.db.Run
+import com.realityexpander.bikingapp.other.Constants.Companion.ACTION_PAUSE_SERVICE
+import com.realityexpander.bikingapp.other.Constants.Companion.ACTION_START_OR_RESUME_SERVICE
+import com.realityexpander.bikingapp.other.Constants.Companion.ACTION_STOP_SERVICE
+import com.realityexpander.bikingapp.other.Constants.Companion.MAP_VIEW_BUNDLE_KEY
+import com.realityexpander.bikingapp.other.Constants.Companion.MAP_ZOOM
+import com.realityexpander.bikingapp.other.Constants.Companion.POLYLINE_COLOR
+import com.realityexpander.bikingapp.other.Constants.Companion.POLYLINE_WIDTH
+import com.realityexpander.bikingapp.other.TrackingUtility
+import com.realityexpander.bikingapp.services.TrackingService
+import com.realityexpander.bikingapp.ui.MainViewModel
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.gms.maps.model.PolylineOptions
+import com.google.android.material.snackbar.Snackbar
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_tracking.*
+import timber.log.Timber
+import java.util.*
+import javax.inject.Inject
+import kotlin.math.round
+
+const val CANCEL_DIALOG_TAG = "CancelDialog"
+
+@AndroidEntryPoint
+class TrackingFragment : Fragment(R.layout.fragment_tracking) {
+
+    @set:Inject
+    var weight: Float = 80f
+
+    private var map: GoogleMap? = null
+
+    private var isTracking = false
+    private var curTimeInMillis = 0L
+    private var pathPoints = mutableListOf<MutableList<LatLng>>()
+
+    private val viewModel: MainViewModel by viewModels()
+
+    private var menu: Menu? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        setHasOptionsMenu(true)
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapViewBundle = savedInstanceState?.getBundle(MAP_VIEW_BUNDLE_KEY)
+        mapView.onCreate(mapViewBundle)
+
+        // restore dialog instance
+        if(savedInstanceState != null) {
+            val cancelRunDialog = parentFragmentManager.findFragmentByTag(CANCEL_DIALOG_TAG) as CancelRunDialog?
+            cancelRunDialog?.setYesListener {
+                stopRun()
+            }
+        }
+
+        btnToggleRun.setOnClickListener {
+            toggleRun()
+        }
+
+        btnFinishRun.setOnClickListener {
+            zoomToWholeTrack()
+            endRunAndSaveToDB()
+        }
+
+        mapView.getMapAsync {
+            map = it
+            addAllPolylines()
+        }
+        subscribeToObservers()
+    }
+
+    /**
+     * Subscribes to changes of LiveData objects
+     */
+    private fun subscribeToObservers() {
+        TrackingService.isTracking.observe(viewLifecycleOwner, Observer {
+            updateTracking(it)
+        })
+
+        TrackingService.pathPoints.observe(viewLifecycleOwner, Observer {
+            pathPoints = it
+            addLatestPolyline()
+            moveCameraToUser()
+        })
+
+        TrackingService.timeRunInMillis.observe(viewLifecycleOwner, Observer {
+            curTimeInMillis = it
+            val formattedTime = TrackingUtility.getFormattedStopWatchTime(it, true)
+            tvTimer.text = formattedTime
+        })
+    }
+
+    /**
+     * Will move the camera to the user's location.
+     */
+    private fun moveCameraToUser() {
+        if (pathPoints.isNotEmpty() && pathPoints.last().isNotEmpty()) {
+            map?.animateCamera(
+                CameraUpdateFactory.newLatLngZoom(
+                    pathPoints.last().last(),
+                    MAP_ZOOM
+                )
+            )
+        }
+    }
+
+    /**
+     * Adds all polylines to the pathPoints list to display them after screen rotations
+     */
+    private fun addAllPolylines() {
+        for (polyline in pathPoints) {
+            val polylineOptions = PolylineOptions()
+                .color(POLYLINE_COLOR)
+                .width(POLYLINE_WIDTH)
+                .addAll(polyline)
+            map?.addPolyline(polylineOptions)
+        }
+    }
+
+    /**
+     * Draws a polyline between the two latest points.
+     */
+    private fun addLatestPolyline() {
+        // only add polyline if we have at least two elements in the last polyline
+        if (pathPoints.isNotEmpty() && pathPoints.last().size > 1) {
+            val preLastLatLng = pathPoints.last()[pathPoints.last().size - 2]
+            val lastLatLng = pathPoints.last().last()
+            val polylineOptions = PolylineOptions()
+                .color(POLYLINE_COLOR)
+                .width(POLYLINE_WIDTH)
+                .add(preLastLatLng)
+                .add(lastLatLng)
+
+            map?.addPolyline(polylineOptions)
+        }
+    }
+
+    /**
+     * Updates the tracking variable and the UI accordingly
+     */
+    private fun updateTracking(isTracking: Boolean) {
+        this.isTracking = isTracking
+        if (!isTracking && curTimeInMillis > 0L) {
+            btnToggleRun.text = getString(R.string.start_text)
+            btnFinishRun.visibility = View.VISIBLE
+        } else if (isTracking) {
+            btnToggleRun.text = getString(R.string.stop_text)
+            menu?.getItem(0)?.isVisible = true
+            btnFinishRun.visibility = View.GONE
+        }
+    }
+
+    /**
+     * Toggles the tracking state
+     */
+    @SuppressLint("MissingPermission")
+    private fun toggleRun() {
+        if (isTracking) {
+            menu?.getItem(0)?.isVisible = true
+            pauseTrackingService()
+        } else {
+            startOrResumeTrackingService()
+            Timber.d("Started service")
+        }
+    }
+
+    /**
+     * Starts the tracking service or resumes it if it is currently paused.
+     */
+    private fun startOrResumeTrackingService() =
+        Intent(requireContext(), TrackingService::class.java).also {
+            it.action = ACTION_START_OR_RESUME_SERVICE
+            requireContext().startService(it)
+        }
+
+    /**
+     * Pauses the tracking service
+     */
+    private fun pauseTrackingService() =
+        Intent(requireContext(), TrackingService::class.java).also {
+            it.action = ACTION_PAUSE_SERVICE
+            requireContext().startService(it)
+        }
+
+    /**
+     * Stops the tracking service.
+     */
+    private fun stopTrackingService() =
+        Intent(requireContext(), TrackingService::class.java).also {
+            it.action = ACTION_STOP_SERVICE
+            requireContext().startService(it)
+        }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        val mapViewBundle = outState.getBundle(MAP_VIEW_BUNDLE_KEY)
+        mapView?.onSaveInstanceState(mapViewBundle!!)
+    }
+
+    /**
+     * Zooms out until the whole track is visible. Used to make a screenshot of the
+     * MapView to save it in the database
+     */
+    private fun zoomToWholeTrack() {
+        val bounds = LatLngBounds.Builder()
+        for (polyline in pathPoints) {
+            for (point in polyline) {
+                bounds.include(point)
+            }
+        }
+        val width = mapView.width
+        val height = mapView.height
+        map?.moveCamera(
+            CameraUpdateFactory.newLatLngBounds(
+                bounds.build(),
+                width,
+                height,
+                (height * 0.05f).toInt()
+            )
+        )
+    }
+
+    /**
+     * Saves the recent run in the Room database and ends it
+     */
+    private fun endRunAndSaveToDB() {
+        map?.snapshot { bmp ->
+            var distanceInMeters = 0
+            for (polyline in pathPoints) {
+                distanceInMeters += TrackingUtility.calculatePolylineLength(polyline).toInt()
+            }
+            val avgSpeed =
+                round((distanceInMeters / 1000f) / (curTimeInMillis / 1000f / 60 / 60) * 10) / 10f
+            val timestamp = Calendar.getInstance().timeInMillis
+            val caloriesBurned = ((distanceInMeters / 1000f) * weight).toInt()
+            val run =
+                Run(bmp, timestamp, avgSpeed, distanceInMeters, curTimeInMillis, caloriesBurned)
+            viewModel.insertRun(run)
+            Snackbar.make(
+                requireActivity().findViewById(R.id.rootView),
+                "Run saved successfully.",
+                Snackbar.LENGTH_LONG
+            ).show()
+            stopRun()
+        }
+    }
+
+    /**
+     * Finishes the tracking.
+     */
+    private fun stopRun() {
+        Timber.d("STOPPING RUN")
+        tvTimer.text = "00:00:00:00"
+        stopTrackingService()
+        findNavController().navigate(R.id.action_trackingFragment_to_runFragment2)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.miCancelTracking -> {
+                showCancelTrackingDialog()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.toolbar_menu_tracking, menu)
+        this.menu = menu
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu) {
+        super.onPrepareOptionsMenu(menu)
+        // just checking for isTracking doesnt trigger this when rotating the device
+        // in paused mode
+        if (curTimeInMillis > 0L) {
+            this.menu?.getItem(0)?.isVisible = true
+        }
+    }
+
+    /**
+     * Shows a dialog to cancel the current run.
+     */
+    private fun showCancelTrackingDialog() {
+        CancelRunDialog().apply {
+            setYesListener {
+                stopRun()
+            }
+        }.show(parentFragmentManager, CANCEL_DIALOG_TAG)
+    }
+
+    override fun onResume() {
+        mapView.onResume()
+        super.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+}
