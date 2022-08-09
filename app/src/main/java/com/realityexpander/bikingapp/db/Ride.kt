84585cb4d2b54eb838ee895bc0b95fd6f6fc1a23
Chris Athanas <realityexpander@gmail.com>
2022-08-08 23:21:41 -0500

    Add Comments

--- app/src/main/java/com/realityexpander/bikingapp/db/Ride.kt
+++ app/src/main/java/com/realityexpander/bikingapp/db/Ride.kt
@@ -7,12 +7,12 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "ride_table")
 data class Ride(
     var img: Bitmap? = null,
-    var timestamp: Long = 0L,
+    var timestamp: Long = 0L,     // date of the ride in milliseconds
     var avgSpeedInKMH: Float = 0f,
     var distanceInMeters: Int = 0,
-    var timeInMillis: Long = 0,
+    var timeInMillis: Long = 0,   // Length of the ride in milliseconds
     var caloriesBurned: Int = 0
 ) {
-    @PrimaryKey(autoGenerate = true)
+    @PrimaryKey(autoGenerate = true)  // room handles generation and uniqueness of primary keys
     var id: Int? = null
 }
